0ba85237d59239e75bcef62647bdf9cc08efa294
root <2643894275@qq.com>
2024-05-28 06:01:18 +0000

    init commit

---
+++ HelloWorldTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloWorldTest{
+    @Test
+    public void testMain(){
+        assertEquals("Hello, World!","Hello, World!");
+    }
+}
